44378873cb533d50244cfe8f537f041ce7bf77c4
jesus <jpo24@gcloud.ua.es>
2022-10-27 17:10:21 +0200

    sockets clima funcionando

--- src/model/AA_Engine.py
+++ src/model/AA_Engine.py
@@ -1,11 +1,19 @@
 import socket
+from sqlite3.dbapi2 import Error 
+import threading
 import sys
+from typing import List
+import sqlite3
+
+FORMAT = 'utf-8'
+FIN = "FIN"
+HEADER = 64
 
 
 
 ########## MAIN ##########
 if  (len(sys.argv) == 1):
-    IP = '127.0.0.6' #puerto de escucha
+    IP = '127.0.0.1' #puerto de escucha
     MAXJUGADORES = 3
     PUERTO_WEATHER = 8080
     ADDR = (IP, PUERTO_WEATHER)
@@ -15,6 +23,20 @@ if  (len(sys.argv) == 1):
     client.connect(ADDR) # nada mas conectarse con el servidor de clima este hace la select de las 4 ciudades
     print (f"Establecida conexión en [{ADDR}]")
 
+    # Recibo un string con 4 ciudades aleatorias y sus temperaturas separadas por comas
+    msg = client.recv(HEADER).decode(FORMAT)
+    lista = msg.split(",")
+    
+    ciudad1 = (lista[0], lista[1])
+    ciudad2 = (lista[2], lista[3])
+    ciudad3 = (lista[4], lista[5])
+    ciudad4 = (lista[6], lista[7])
+    print(ciudad1)
+
+
+    print(msg)
+    # Recibo un mensaje del cliente
+    print(f" He recibido del cliente [{ADDR}] el mensaje: {msg}")
     
 
             
--- src/model/AA_Weather.py
+++ src/model/AA_Weather.py
@@ -6,28 +6,56 @@ import sqlite3
 
 # Variables globales constantes
 HEADER = 64
-SERVER = '127.0.0.6'
+SERVER = '127.0.0.1'
 FORMAT = 'utf-8'
 FIN = "FIN"
 
+def send(msg, client):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
 
 
-def enviarCiudades(conn, addr):
-    # Conexión a la bd
-        connection = sqlite3.connect('clima.db')
+def enviarCiudades(ip, puerto):
+    ADDR = (ip, int(puerto))
 
-        # Cursor para realizar los comandos de la bd
-        cursor = connection.cursor()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print("[STARTING] AA_WEATHER inicializándose...")
+    print(f"[LISTENING] AA_WEATHER a la escucha en {SERVER}")
 
+    while True:
+        conn, addr = server.accept()
+
+        try:
+            # Conexión a la bd
+            connection = sqlite3.connect('clima.db')
+
+            # Cursor para realizar los comandos de la bd
+            cursor = connection.cursor()
+
+            # Comando para extraer 4 ciudades aleatoriamente de la base de datos
+            command = "select * from cities order by random() limit 4;"
+            cursor.execute(command)
+            
+            # Al hacer fetcall en resultado se guardaría una lista de tuplas con las 4 ciudades
+            resultado = cursor.fetchall()
+            
+            cadena = ""
+            for ciudad in resultado:
+                cadena += str(ciudad[0]) + "," +  str(ciudad[1]) + ","
+        except Error as e:
+            print("Algo falló") 
+        finally:
+            cursor.close()
+            connection.close()
         
-         # Comando para extraer 4 ciudades aleatoriamente de la base de datos
-        command = "select * from cities order by random() limit 4;"
-        cursor.execute(command)
-        
-        # Al hacer fetcall en resultado se guardaría una lista de tuplas con las 4 ciudades
-        resultado = cursor.fetchall()
-        
-        print("Enviadas ciudades") 
+        conn.send(str(cadena).encode(FORMAT))
+        conn.close()
 
 
 
@@ -38,18 +66,20 @@ if len(sys.argv) == 1:
     PORT = 8080 #sys.argv[1]
     ADDR = (SERVER, int(PORT))
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
 
-    print("[STARTING] Inicializando AA_Weather...")
-    server.listen()
-    print(f"[LISTENING] Servidor a la escucha en {SERVER}")
+    #server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #server.bind(ADDR)
 
-    while True:
-        conn, addr = server.accept()
+    #print("[STARTING] Inicializando AA_Weather...")
+    #server.listen()
+    #print(f"[LISTENING] Servidor a la escucha en {SERVER}")
 
-        thread = threading.Thread(target=enviarCiudades, args=(conn, addr))
-        thread.start()
+    
+        #conn, addr = server.accept()
+
+    thread = threading.Thread(target=enviarCiudades, args=(SERVER, int(PORT)))
+
+    thread.start()
 else:
     print("Error en los parámetros: AA_Weather.py <puerto escucha>")
 
