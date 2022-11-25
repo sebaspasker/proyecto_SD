3c628970c9ca5d2abfe2e8b630b62c66721d4c05
Sebastian Pasker <spg84@gcloud.ua.es>
2022-11-25 11:38:03 +0100

    Lab json

--- scripts/create_json_cfg.py
+++ scripts/create_json_cfg.py
@@ -18,6 +18,24 @@ data = {
     "RESET": "YES",
 }
 
-with open("./config/config.json", "w") as outfile:
-    str_ = json.dumps(data)
+# COPIAR CUANDO CAMBIEMOS PARÁMETROS
+data_lab = {
+    "IP_ENGINE": "172.20.40.175",
+    "PORT_ENGINE": 5050,
+    "IP_REG": "172.20.40.175",
+    "PORT_REG": 5051,
+    "IP_WEATHER": "172.20.40.174",
+    "PORT_WEATHER": 5052,
+    "KAFKA_SERVER": "172.20.40.174:9092",
+    "FORMAT": "utf-8",
+    "DB_SERVER": "againstall.db",
+    "MAX_USERS": 4,
+    "TIME_WAIT_SEC": 60,
+    "HEADER": 64,
+    "NUMBER_NPC": 3,
+    "RESET": "NO",
+}
+
+with open("./config/config_lab.json", "w") as outfile:
+    str_ = json.dumps(data_lab)
     outfile.write(str_)
