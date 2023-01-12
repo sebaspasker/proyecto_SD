f30f1f0604bbff6de6c61e4a5362e6bf9b3c2c31
Sebastian Pasker <spg84@gcloud.ua.es>
2023-01-12 11:26:52 +0100

    A

--- scripts/create_json_cfg.py
+++ scripts/create_json_cfg.py
@@ -2,17 +2,17 @@ import json
 
 # COPIAR CUANDO CAMBIEMOS PARÁMETROS
 data = {
-    "IP_ENGINE": "127.0.0.1",
+    "IP_ENGINE": "172.20.40.175",
     "PORT_ENGINE": 5050,
-    "IP_REG": "127.0.0.2",
+    "IP_REG": "172.20.40.175",
     "PORT_REG": 5051,
     "IP_WEATHER": "127.0.0.3",
     "PORT_WEATHER": 5052,
-    "KAFKA_SERVER": "localhost:9092",
+    "KAFKA_SERVER": "192.20.40.174:9092",
     "FORMAT": "utf-8",
     "DB_SERVER": "againstall.db",
     "MAX_USERS": 4,
-    "TIME_WAIT_SEC": 10,
+    "TIME_WAIT_SEC": 30,
     "HEADER": 64,
     "NUMBER_NPC": 4,
     "RESET": "NO",
@@ -38,6 +38,6 @@ data_lab = {
     "RESET": "NO",
 }
 
-with open("./config/config.json", "w") as outfile:
+with open("./config/config_lab_2.json", "w") as outfile:
     str_ = json.dumps(data)
     outfile.write(str_)
